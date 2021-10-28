ccb5432751c87da80db1a7bb3af079f83501a1ab
Md Arifur Rahman <rahman1193arif@gmail.com>
2021-10-29 03:12:17 +0600

    react router worked

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>This is Header</h2>
+           <div className="header-main">
+           <nav>
+                <Link to="/">Home</Link>
+                <Link to="/ourServices">Our Services</Link>
+                {/* <Link to="/users/add">Add User</Link> */}
+            </nav>
+           </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/OurServices/OurServices.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurServices = () => {
+    return (
+        <div>
+            <h2>this is our service</h2>
+        </div>
+    );
+};
+
+export default OurServices;
