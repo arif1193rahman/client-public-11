070cbbb1050650f6475da7f74e53b071b040d291
Md Arifur Rahman <rahman1193arif@gmail.com>
2021-11-04 00:36:36 +0600

    nice

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -7,12 +7,13 @@ const Banner = () => {
     <div>
       <div className="hero-image">
         <Image src="https://i.ibb.co/8dmHYQ4/maxresdefault-1.jpg" fluid />
-        
       </div>
       <div className="hero-text">
-          <h1 className="banner-text" style={{ fontSize: "50px" }}>I am John Doe</h1>
-          <p className="banner-second">And I'm a Your Guid</p>
-        </div>
+        <h1 className="banner-text" style={{ fontSize: "50px" }}>
+          I am Arif Rahman
+        </h1>
+        <p className="banner-second">And I'm a Your Guid</p>
+      </div>
     </div>
   );
 };
